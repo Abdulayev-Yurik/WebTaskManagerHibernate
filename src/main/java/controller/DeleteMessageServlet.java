30b62605e06e8b2eae9f243cc2faf406ed7fe423
Yurik Abdulayev <yuriia@softwareplanet.com>
2016-11-18 16:38:16 +0200

    add dependencies, add delete servlet

---
+++ src/main/java/controller/DeleteMessageServlet.java
@@ -0,0 +1,30 @@
+package controller;
+
+import dao.TaskDAO;
+import org.hibernate.SessionFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by employee on 11/15/16.
+ */
+@WebServlet({"/delMessages"})
+public class DeleteMessageServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String messageId = req.getParameter("messageId");
+        String taskId = req.getParameter("taskId");
+
+        TaskDAO taskDAO = new TaskDAO((SessionFactory) getServletContext().getAttribute("factory"));
+        taskDAO.deleteMessage(messageId);
+        resp.sendRedirect("/details?task=" + taskId);
+    }
+}
