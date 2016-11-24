6c83e76cc1ec4228dcf9b4b8187c0bc7c67dbb75
Yurik Abdulayev <yurs11k89@gmail.com>
2016-11-24 21:45:07 +0200

    create message controller

---
+++ src/main/java/taskmanager/controller/MessageController.java
@@ -0,0 +1,31 @@
+package taskmanager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import taskmanager.service.MessageService;
+
+/**
+ * Created by employee on 11/24/16.
+ */
+@Controller
+@RequestMapping("/message")
+public class MessageController {
+
+    @Autowired
+    MessageService messageService;
+
+    @RequestMapping(value = "", method = RequestMethod.DELETE)
+    String deleteMessage(@RequestParam("messageId")String mId, @RequestParam("taskId")String taskId){
+        messageService.deleteMessage(mId);
+        return "redirect:/details?taskId=" + taskId;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    String createMessage(@RequestParam("taskId")String taskId, @RequestParam("mBody")String messageBody){
+        messageService.createMessage(taskId, messageBody);
+        return "redirect:/details?taskId=" + taskId;
+    }
+}
