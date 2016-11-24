5f68c1728c5528700a86f16378314924a38d975a
Yurik Abdulayev <yurs11k89@gmail.com>
2016-11-24 21:39:39 +0200

    add spring mvc config with hibernate

---
+++ src/main/java/taskmanager/controller/DetailsController.java
@@ -0,0 +1,26 @@
+package taskmanager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import taskmanager.service.TaskService;
+
+/**
+ * Created by employee on 11/24/16.
+ */
+@Controller
+@RequestMapping("/details")
+public class DetailsController {
+
+    @Autowired
+    TaskService taskService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    String viewDetails(@RequestParam("taskId")String taskId, ModelMap modelMap){
+        modelMap.addAttribute("task", taskService.getTaskById(taskId));
+        return "details";
+    }
+}
