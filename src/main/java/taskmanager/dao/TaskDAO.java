9601f09818023d32a59affeaa14d7d932041d784
Yurik Abdulayev <yurs11k89@gmail.com>
2016-11-24 21:36:48 +0200

    create task dao

---
+++ src/main/java/taskmanager/dao/TaskDAO.java
@@ -0,0 +1,81 @@
+package taskmanager.dao;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import taskmanager.model.Message;
+import taskmanager.model.Task;
+
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+import static org.hibernate.criterion.Restrictions.eq;
+
+/**
+ * Created by employee on 11/16/16.
+ */
+@Repository
+@Transactional
+public class TaskDAO {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    public List<Task> getAllTasks(){
+        Criteria criteria = sessionFactory.getCurrentSession()
+                .createCriteria(Task.class);
+        List<Task> list = (List<Task>) criteria.list();
+        return list;
+    }
+
+    public List<Task> getTasksFromList(String listId) {
+        if (listId.equals("0"))
+            return getAllTasks();
+        Criteria criteria = sessionFactory.getCurrentSession()
+                .createCriteria(Task.class)
+                .add(eq("listId", parseInt(listId)));
+        List<Task> taskList = (List<Task>) criteria.list();
+        return taskList;
+    }
+
+
+    public void addNewTask(String title, String details, int listId, String dueDate) {
+        Task task = new Task(title, details, listId, dueDate, true);
+        sessionFactory.getCurrentSession().save(task);
+    }
+
+    public void deleteTask(String taskId) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Task task = (Task) currentSession.createCriteria(Task.class)
+                .add(eq("id", taskId)).uniqueResult();
+        currentSession.delete(task);
+    }
+
+    public void switchStatusTask(String taskId, boolean isActive) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Task task = (Task) currentSession.createCriteria(Task.class)
+                .add(eq("id", taskId)).uniqueResult();
+        task.setActive(isActive);
+        currentSession.update(task);
+    }
+
+    public Task getTaskById(String taskId) {
+        Task task = (Task) sessionFactory.getCurrentSession()
+                .createCriteria(Task.class)
+                .add(eq("id", taskId))
+                .uniqueResult();
+        return task;
+    }
+
+    private List<Message> getTaskMessages(String taskId) {
+        Task task = (Task) sessionFactory.getCurrentSession()
+                .createCriteria(Task.class)
+                .add(eq("id", taskId))
+                .uniqueResult();
+        return task.getMessages();
+    }
+
+}
