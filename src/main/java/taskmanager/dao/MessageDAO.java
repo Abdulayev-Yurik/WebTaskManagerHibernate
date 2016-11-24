a5284ebc35314302bb554b74e06d7b234b6f6c01
Yurik Abdulayev <yurs11k89@gmail.com>
2016-11-24 21:37:15 +0200

    create message dao

---
+++ src/main/java/taskmanager/dao/MessageDAO.java
@@ -0,0 +1,44 @@
+package taskmanager.dao;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import taskmanager.model.Message;
+import taskmanager.model.Task;
+
+import java.sql.SQLException;
+
+import static java.lang.Integer.parseInt;
+import static org.hibernate.criterion.Restrictions.eq;
+
+/**
+ * Created by employee on 11/17/16.
+ */
+@Repository
+@Transactional
+public class MessageDAO {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    public void addNewMessage(String taskId, String messageText) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Message message = new Message(parseInt(taskId), messageText);
+        currentSession.save(message);
+        Task task = (Task) currentSession.createCriteria(Task.class)
+                .add(eq("id", taskId))
+                .uniqueResult();
+        task.getMessages().add(message);
+        currentSession.save(task);
+    }
+
+    public void deleteMessage(String messageId) {
+        Session session = sessionFactory.getCurrentSession();
+        Criteria criteria = session.createCriteria(Message.class).add(eq("id", parseInt(messageId)));
+        Message message = (Message) criteria.uniqueResult();
+        session.delete(message);
+    }
+}
