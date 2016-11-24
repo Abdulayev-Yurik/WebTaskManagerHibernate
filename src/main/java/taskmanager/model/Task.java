495eec45e9392bf8d77b0a0ca5f1b6a8af826cae
Yurik Abdulayev <yurs11k89@gmail.com>
2016-11-24 21:36:00 +0200

    add some entity

---
+++ src/main/java/taskmanager/model/Task.java
@@ -0,0 +1,147 @@
+package taskmanager.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by employee on 11/15/16.
+ */
+@Entity
+@Table(name = "task_table")
+public class Task {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String taskId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "listId", insertable = false, updatable = false )
+    private TaskList listTasks;
+
+    @Column(name = "title")
+    private String taskTitle;
+
+    @Column(name = "details")
+    private String details;
+
+    @Column(name = "due_date")
+    private String dueDate;
+
+    @Column(name = "isActive", columnDefinition = "true")
+    private Boolean isActive;
+
+    @Column(name = "listId")
+    private Integer listId;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "task")
+    private List<Message> messages = new ArrayList();
+
+    public Task(String taskId, String taskTitle) {
+        this.taskId = taskId;
+        this.taskTitle = taskTitle;
+        isActive = true;
+    }
+
+    public Task(String taskId, String taskTitle, String details, boolean isActive) {
+        this.taskId = taskId;
+        this.taskTitle = taskTitle;
+        this.details = details;
+        this.isActive = isActive;
+    }
+
+    public Task(String taskId, String taskTitle, String details, boolean isActive, int listId) {
+        this.taskId = taskId;
+        this.taskTitle = taskTitle;
+        this.details = details;
+        this.isActive = isActive;
+        this.listId = listId;
+    }
+
+    public Task(String taskTitle, String details, boolean isActive, Integer listId) {
+        this.taskTitle = taskTitle;
+        this.details = details;
+        this.isActive = isActive;
+        this.listId = listId;
+    }
+
+    public Task() {
+    }
+
+    public Task(String title, String details, int listId, String dueDate, boolean isActive) {
+        this.taskTitle = title;
+        this.details = details;
+        this.listId = listId;
+        this.dueDate = dueDate;
+        this.isActive = isActive;
+    }
+
+    @Column(name = "title")
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public Integer getMessagesValues() {
+        return messages.size();
+    }
+
+    @Column(name = "id")
+    public String getTaskId() {
+        return taskId;
+    }
+
+    @Column(name = "details")
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Column(name = "isActive", columnDefinition = "tinyint default true")
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public int getListId() {
+        return listId;
+    }
+
+    public void setListId(int listId) {
+        this.listId = listId;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public TaskList getListTasks() {
+        return listTasks;
+    }
+
+    public void setListTasks(TaskList listTasks) {
+        this.listTasks = listTasks;
+    }
+}
---
+++ src/main/java/taskmanager/model/TaskList.java
@@ -0,0 +1,60 @@
+package taskmanager.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yurik on 16.11.16.
+ */
+@Entity
+@Table(name = "list_tasks")
+public class TaskList {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String listName;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "listTasks")
+    private List<Task> tasks = new ArrayList();
+
+    public TaskList(int id, String listName) {
+        this.id = id;
+        this.listName = listName;
+    }
+
+    public TaskList(String listName) {
+        this.listName = listName;
+    }
+
+    public TaskList() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
