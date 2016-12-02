d01ddf8089db909e4e37a94f205ace8db247be1b
Yurik Abdulayev <yurs11k89@gmail.com>
2016-12-02 19:29:01 +0200

    create template to rest controllers

---
+++ src/main/java/rest/taskmanager/MessageController.java
@@ -0,0 +1,13 @@
+package rest.taskmanager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by yurik on 02.12.16.
+ */
+@Controller
+@RequestMapping("/api/message")
+public class MessageController {
+
+}
