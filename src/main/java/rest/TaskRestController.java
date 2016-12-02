fc5003c4f99f7c83774b1237fa9d80c76ac1c3f3
Yurik Abdulayev <yurs11k89@gmail.com>
2016-12-02 22:32:36 +0200

    add methods to task rest

--- src/main/java/rest/TaskRestController.java
+++ src/main/java/rest/TaskRestController.java
@@ -2,9 +2,7 @@ package rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import taskmanager.model.Task;
 import taskmanager.service.TaskService;
 
@@ -18,10 +16,44 @@ public class TaskRestController {
     @Autowired
     TaskService taskService;
 
-    @RequestMapping("/{taskId}")
     @ResponseBody
+    @RequestMapping("/{taskId}")
     Task getTask(@PathVariable("taskId") Integer id) {
         return taskService.getTaskById(id);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    String createTask(@RequestParam("title") String title,
+                      @RequestParam("details") String details,
+                      @RequestParam("listId") Integer listId,
+                      @RequestParam("dueDate") String dueDate) {
+        taskService.addNewTask(title, details, listId, dueDate);
+        return "";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{taskId}", method = RequestMethod.DELETE)
+    String deleteTask(@PathVariable("taskId") Integer taskId) {
+        taskService.deleteTask(taskId);
+        return "";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{taskId}/status/{status}", method = RequestMethod.PUT)
+    String switchStatusTask(@PathVariable("taskId") Integer taskId) {
+        taskService.switchTaskActivity(taskId);
+        return "{status:\"done\"}";
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.PATCH)
+    String updateTask(@RequestParam("id")Integer id,
+                    @RequestParam("title")String title,
+                    @RequestParam("details")String det,
+                    @RequestParam("date")String dueDate){
+        taskService.updateTask(id, title, det, dueDate);
+        return "{status:\"done\"}";
     }
 
 }
